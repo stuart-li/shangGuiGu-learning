9c016db6f1b525b2896f8a64b2a09e96aea4086e
root <root@163.com>
2022-04-16 23:28:35 +0800

    test

---
+++ src/test20220416/Test.java
@@ -0,0 +1,17 @@
+package test20220416;
+
+import java.util.Scanner;
+
+/**
+ * @Param:
+ * @Author:
+ * @Date:2022/4/16 23:20
+ * @Version:1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("ÄúÊÇÖí");
+    }
+
+}
